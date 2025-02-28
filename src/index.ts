81ad443044e46407f038357bdae9bfb0013a0809
Palese Mirco (E) <mirco.palese@mermecgroup.com>
2025-03-01 00:53:34 +0100

    update .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * as Space from  './models/space';
-export * as execute from './script' 
+// export * as execute from './script' 
 export {AreaEvents} from './types/area-events'
--- src/script.ts
+++ src/script.ts
@@ -1,63 +1,63 @@
 
 
-import Area from './models/area';
-import Space from './models/space';
+// import Area from './models/area';
+// import Space from './models/space';
 
-const execute = () => {
-  const container = document.querySelector('#mainDiv') as HTMLElement;
-  const button = document.querySelector('#newDiv');
+// const execute = () => {
+//   const container = document.querySelector('#mainDiv') as HTMLElement;
+//   const button = document.querySelector('#newDiv');
 
-  console.log('Space importato:', Space);
-  console.log('Tipo di Space:', typeof Space);
-  console.log('Container:', container);
+//   console.log('Space importato:', Space);
+//   console.log('Tipo di Space:', typeof Space);
+//   console.log('Container:', container);
 
-  const space = new Space(container);
-  console.log('new space created')
+//   const space = new Space(container);
+//   console.log('new space created')
 
-  button.addEventListener('click', () => {
-    const area = space.createArea();
-    console.log('new area clicked')
+//   button.addEventListener('click', () => {
+//     const area = space.createArea();
+//     console.log('new area clicked')
 
-    // // select test 
-    area.on('select', e => {
-      const area = e.target as Area;
-      const resizableEl = area.getResizable() 
-      resizableEl.style.backgroundColor = 'blue';
-    });
-    area.on('deselect', e => {
-      const area = e.target as Area;
-      const resizableEl = area.getResizable() 
-      resizableEl.style.backgroundColor = 'red';
-    });
+//     // // select test 
+//     area.on('select', e => {
+//       const area = e.target as Area;
+//       const resizableEl = area.getResizable() 
+//       resizableEl.style.backgroundColor = 'blue';
+//     });
+//     area.on('deselect', e => {
+//       const area = e.target as Area;
+//       const resizableEl = area.getResizable() 
+//       resizableEl.style.backgroundColor = 'red';
+//     });
 
-    // delete test
-    area.on('before-delete', ()=>{
-      console.log('before delete')
-    })
-    area.on('after-delete', ()=>{
-      console.log('afterdelete')
-    })
+//     // delete test
+//     area.on('before-delete', ()=>{
+//       console.log('before delete')
+//     })
+//     area.on('after-delete', ()=>{
+//       console.log('afterdelete')
+//     })
 
-    // resize test
-    area.on('resize', (e)=>{
-      console.log('resize', e)
-    })
-    area.on('resize-start', (e)=>{
-      console.log('resize-start', e)
-    })
-    area.on('resize-end', (e)=>{
-      console.log('resize-end', e)
-    })
+//     // resize test
+//     area.on('resize', (e)=>{
+//       console.log('resize', e)
+//     })
+//     area.on('resize-start', (e)=>{
+//       console.log('resize-start', e)
+//     })
+//     area.on('resize-end', (e)=>{
+//       console.log('resize-end', e)
+//     })
 
-    area.on('move', (e)=>{
-      console.log('move', e)
-    })
-
-
-
-  });
+//     area.on('move', (e)=>{
+//       console.log('move', e)
+//     })
 
 
-};
 
-export default execute;
+//   });
+
+
+// };
+
+// export default execute;
